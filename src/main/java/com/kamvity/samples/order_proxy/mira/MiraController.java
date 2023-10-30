68060f073dc4d650424ea95e24d2c3d3d6674e4c
Mathieu C <mathieu.kamvity@gmail.com>
2023-10-30 22:59:14 +0100

    add controller

--- src/main/java/com/kamvity/samples/order_proxy/mira/MiraController.java
+++ src/main/java/com/kamvity/samples/order_proxy/mira/MiraController.java
@@ -3,13 +3,11 @@ package com.kamvity.samples.order_proxy.mira;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,11 +16,14 @@ import java.util.Optional;
 public class MiraController {
     private final MiraSubOrderService miraSubOrderService;
     private static final Logger log = LoggerFactory.getLogger(MiraController.class);
-    @GetMapping("/get_order_id")
+    @GetMapping("/get-order-id")
     public Mono<HashMap> getOrderById(@RequestParam Optional<String> id) {
         log.debug(String.format("get_order_id,id=%s",id.get()));
         return miraSubOrderService.getOrderById(id);
     }
 
-
+    @PostMapping("/set-order")
+    public Mono<HashMap> setOrder(@RequestBody Optional<List<HashMap>> orders) {
+        return miraSubOrderService.setOrder(orders);
+    }
 }
--- src/test/java/com/kamvity/samples/order_proxy/mira/MiraControllerTest.java
+++ src/test/java/com/kamvity/samples/order_proxy/mira/MiraControllerTest.java
@@ -6,6 +6,8 @@ import com.kamvity.samples.order_proxy.config.OrderProxyConfig;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.tracing.Tracer;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,7 +31,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Hooks;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
@@ -75,20 +80,9 @@ public class MiraControllerTest {
 
     String order1 = "{\n" +
             "    \"orderId\": 1,\n" +
-            "    \"price\": 200.0,\n" +
-            "    \"orderTimestamp\": \"2023-01-09T14:12:13.931+00:00\",\n" +
-            "    \"customer\": {\n" +
-            "        \"customerId\": 1,\n" +
-            "        \"title\": \"Majesty\",\n" +
-            "        \"firstname\": \"Soma\",\n" +
-            "        \"lastname\": \"Leavyi\",\n" +
-            "        \"email\": \"soma.leavyi@email.org\",\n" +
-            "        \"address\": \"1 street of Majesty, Cambodia\",\n" +
-            "        \"zipCode\": \"2222\"\n" +
-            "    }\n" +
+            "    \"orderTimestamp\": \"2023-01-09T14:12:13.931+00:00\"\n" +
             "}";
 
-    String order2 = "{ \"order\" : \"soma\" }";
 
     public void mockOrderGetByIdOK() {
         Header header = Header.header("Content-Type","application/json");
@@ -96,7 +90,26 @@ public class MiraControllerTest {
                 .when(
                         request()
                                 .withMethod("GET")
-                                .withPath("/v1/order-terminal/get-by-id")
+                                .withPath("/v1/mira/get-by-id")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withHeader(header)
+                                .withBody(order1)
+                );
+    }
+    public void mockOrderSetOK(List<HashMap> orders) {
+        Header header = Header.header("Content-Type","application/json");
+        List<JSONObject> jsonObjectList = new ArrayList<JSONObject>();
+        orders.stream().forEach((o) -> jsonObjectList.add(new JSONObject(o)));
+        JSONArray jsonArray = new JSONArray(jsonObjectList);
+        new MockServerClient("127.0.0.1",8090)
+                .when(
+                        request()
+                                .withMethod("POST")
+                                .withPath("/v1/mira/set-order")
+                                .withBody(jsonArray.toString())
                 )
                 .respond(
                         response()
@@ -107,12 +120,29 @@ public class MiraControllerTest {
     }
 
 
-
     @Test
     public void testGetOrderId() {
         mockOrderGetByIdOK();
-        String url = "http://localhost:" + port+"/api/v1/mira/orders/get_order_id?id=1";
+        String url = "http://localhost:" + port+"/api/v1/mira/orders/get-order-id?id=1";
         ResponseEntity<HashMap> re = restTemplate.getForEntity(url, HashMap.class);
         assertEquals(HttpStatus.OK,re.getStatusCode());
+        assertEquals(1,re.getBody().get("orderId"));
+    }
+
+    @Test
+    public void testSetOrderId() {
+
+        String url = "http://localhost:" + port+"/api/v1/mira/orders/set-order";
+        List<HashMap> orders = new ArrayList<>();
+        HashMap<String,String> order = new HashMap<>();
+        order.put("productId","123");
+        order.put("quantity","1");
+        order.put("deliveryDueDate","2023-10-23");
+        orders.add(order);
+        mockOrderSetOK(orders);
+        HttpEntity<List<HashMap>> ordersEntity = new HttpEntity<>(orders);
+        ResponseEntity<HashMap> re = restTemplate.postForEntity(url, ordersEntity, HashMap.class);
+        assertEquals(HttpStatus.OK,re.getStatusCode());
+        assertEquals(1,re.getBody().get("orderId"));
     }
 }
